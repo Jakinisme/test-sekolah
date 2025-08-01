8c68d3bcdc7ba0a681126fcd0cb1d996fe06ef9b
Jaki <adwashark@gmail.com>
2025-08-01 23:28:53 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Footer() {
+export default function Footer({ name }) {
     return (
         <>
             <footer className='footer'>
                 <div className='footer-content'>
                     <div className='footer-text'>
-                        © 2023 Your Company Name. All rights reserved.
+                        © 2023 {name}. All rights reserved.
                     </div>
                 </div>
             </footer>
