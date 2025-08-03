c3540ea3554fb7ce0080950501d4770b2d823125
Jaki <adwashark@gmail.com>
2025-08-03 20:01:03 +0700

    minor

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,14 +3,12 @@
 export default function Home(){
     return(
         <>
-        <div className="main">
-            <div className ="home">            
+        <div className="main">          
             <div className ="home-container">
                 <div className ="home-content">
                     ini home
                 </div>
             </div>
-        </div>
         </div>
         </>
     )
--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -4,15 +4,13 @@
 export default function ProfilSekolah(){
     return(
         <>
-        <div className="main">
-            <div className ="profile">            
+        <div className="main">            
             <div className ="profile-container">
                 <div className ="profile-content">
                     ini profil
                 </div>
             </div>
-        </div>
-        </div>  
+        </div> 
         </>
     )
 }
