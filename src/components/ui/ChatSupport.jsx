859f697ee1d7ea2437621402ad45485af87303d6
Jaki <adwashark@gmail.com>
2025-08-15 19:09:07 +0700

    chat

--- src/components/ui/ChatSupport.jsx
+++ src/components/ui/ChatSupport.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 import { Bot } from "lucide-react"
+import { useEffect } from 'react'
 
 export default function ChatSupport() {
-    
+    const [isChatOpen, setIsChatOpen] = useState(false)
 
+    const toggleChat = (toggle) => {
+      return setIsChatOpen(toggle)
+    }
+
+    useEffect(() => {
+        if (isChatOpen) {
+            document.body.classList.add('chat-opened')
+        } else {
+            document.body.classList.remove('chat-opened')
+        }
+
+        return () => {
+            document.body.classList.remove('chat-opened')
+        } 
+    }, [isChatOpen])
 
     return (
-        <></>
+        <div className=''>
+
+        </div>
     )
 }
